f86c90a07fa90f291893347ce77bf86647a41463
Javier Mora <cousteaulecommandant@gmail.com>
2024-11-25 21:14:26 +0100

    (System)Verilog: escaped identifiers (LRM 5.6.1)
    Add support for escaped identifiers in Verilog and SystemVerilog:
    `\` + zero or more non-whitespace characters + whitespace.
    
    Note that the `\` itself isn't part of the identifier, and that `\foo`
    is the same as just `foo` (unlike in VHDL), but that identifiers
    identical to keywords such as `\begin` are also allowed.
    This definition also theoretically allows an empty identifier, `\`,
    which ctags currently ignores.
    
    Escaped identifiers are defined in the standard in section
    3.7.1 (Verilog) / 5.6.1 (SystemVerilog).

---
+++ Units/parser-verilog.r/verilog-escaped-id.d/input.v
@@ -0,0 +1,12 @@
+module \themodule#(x=42) (
+    input wire \clk,rst, , \d ,
+    output reg \1+1=2
+);
+
+localparam \begin = 1'b1;
+
+always @(posedge \clk,rst, ) begin : \\r\n
+    \1+1=2 <= \d ^ \begin ;
+end
+
+endmodule
